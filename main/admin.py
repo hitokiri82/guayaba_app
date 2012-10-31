bb68b26b0eca4858eb2eea1bcc70156ecca78943
Francisco Caraballo <francisco.caraballo@gmail.com>
2012-10-31 22:03:07 +0100

    First batch of important work

---
+++ main/admin.py
@@ -0,0 +1,30 @@
+from main.models import NaturalClient, LegalClient, Case, Firm, Event
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from main.models import UserProfile
+
+
+admin.site.register(Case)
+admin.site.register(LegalClient)
+admin.site.register(NaturalClient)
+admin.site.register(Firm)
+admin.site.register(Event)
+
+
+# Define an inline admin descriptor for UserProfile model
+# which acts a bit like a singleton
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
---
+++ main/models.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.utils.translation import ugettext_lazy as _
+
+
+NAT_ID_TYPES = (
+    (u'PSP', _(u'Pasaporte')),
+    (u'NIE', _(u'NIE')),
+    (u'CIF', _(u'CIF')),
+)
+
+CORP_ID_TYPES = (
+    (u'CIF', _(u'CIF')),
+)
+
+
+class Client(models.Model):
+    class Meta:
+        verbose_name = _('Cliente')
+        verbose_name_plural = _('Clientes')
+
+    client_type = models.CharField(_('Tipo de Cliente'), max_length=1)
+    phone_1 = models.CharField(_('Telefono 1'), max_length=20, blank=True, null=True)
+    phone_2 = models.CharField(_('Telefono 2'), max_length=20, blank=True, null=True)
+    phone_3 = models.CharField(_('Telefono 3'), max_length=20, blank=True, null=True)
+    street = models.CharField(_('Calle/Av.'), max_length=20, blank=True, null=True)
+    postal_zone = models.CharField(_('Zona Postal'), max_length=20, blank=True, null=True)
+    country = models.CharField(_('Pais'), max_length=20, blank=True, null=True)
+    city = models.CharField(_('Ciudad'), max_length=20, blank=True, null=True)
+    state = models.CharField(_('Estado/Provincia'), max_length=20, blank=True, null=True)
+
+    def __unicode__(self):
+        return unicode(self.id)
+
+
+class NaturalClient(Client):
+    class Meta:
+        verbose_name = _('Cliente Natural')
+        verbose_name_plural = _('Clientes Naturales')
+
+    name = models.CharField(_('Nombre'), max_length=25)
+    name_2 = models.CharField(_('Segundo Nombre'), max_length=25, blank=True, null=True)
+    last_name = models.CharField(_('Apellido'), max_length=25)
+    last_name_2 = models.CharField(_('Segundo Apellido'), max_length=25, blank=True, null=True)
+    id_type = models.CharField(_('Tipo de ID'), max_length=3, choices=NAT_ID_TYPES)
+    id_number = models.CharField(_('Numero de ID'), max_length=15)
+    email = models.CharField(_('E-mail'), max_length=50, blank=True, null=True)
+
+    def __unicode__(self):
+        return unicode(self.last_name + self.name)
+
+
+class LegalClient(Client):
+    class Meta:
+        verbose_name = _('Cliente Juridico')
+        verbose_name_plural = _('Clientes Juridicos')
+
+    corporate_name = models.CharField(_('Razon Social'), max_length=50)
+    contact_last_name = models.CharField(_('Apellido de persona de contacto'), max_length=25, blank=True, null=True)
+    contact_first_name = models.CharField(_('Nombre de persona de contacto'), max_length=25, blank=True, null=True)
+    contact_phone_number = models.CharField(_('Telefono de persona de contacto'), max_length=20, blank=True, null=True)
+    contact_email = models.CharField(_('E-mail de persona de contacto'), max_length=50, blank=True, null=True)
+    id_type = models.CharField(_('Tipo de ID'), max_length=3, choices=CORP_ID_TYPES)
+    id_number = models.CharField(_('Numero de ID'), max_length=15)
+
+    def __unicode__(self):
+        return unicode(self.corporate_name)
+
+
+class Case(models.Model):
+    class Meta:
+        verbose_name = _('Caso')
+        verbose_name_plural = _('Casos')
+
+    client = models.ForeignKey(Client)
+    created_date = models.DateField(_('Fecha de Creación'), auto_now_add=True)
+    begin_date = models.DateField(_('Fecha Inicio'), blank=True, null=True)
+    end_date = models.DateField(_('Fecha Fin'), blank=True, null=True)
+    description = models.CharField(_('Descripcion'), max_length=50)
+    status = models.CharField(_('Estado/Status'), max_length=2, blank=True, null=True)
+    result = models.CharField(_('Resultado'), max_length=2, blank=True, null=True)
+    comments = models.TextField(_('Comentarios'), blank=True, null=True)
+    budget = models.DecimalField(_('Presupuesto'), max_digits=10, decimal_places=2)
+    collected = models.DecimalField(_('Monto Cobrado'), max_digits=10, decimal_places=2)
+    currency = models.CharField(_('Moneda'), max_length=3, blank=True, null=True)
+
+    def __unicode__(self):
+        return unicode(self.id)
+
+
+class Event(models.Model):
+    class Meta:
+        verbose_name = _('Evento')
+        verbose_name_plural = _('Eventos')
+
+    case = models.ForeignKey(Case)
+    date = models.DateField(_('Fecha'), blank=True, null=True)
+    begin_time = models.TimeField(_('Hora de Inicio'), blank=True, null=True)
+    end_time = models.TimeField(_('Hora Fin'), blank=True, null=True)
+    description = models.CharField(_('Descripcion'), max_length=50)
+    comments = models.TextField(_('Comentarios'), blank=True, null=True)
+    status = models.CharField(_('Estado/Status'), max_length=2, blank=True, null=True)
+    result = models.CharField(_('Resultado'), max_length=2, blank=True, null=True)
+
+    def __unicode__(self):
+        pass
+
+
+class Firm(models.Model):
+        class Meta:
+            verbose_name = _('Despacho')
+            verbose_name_plural = _('Despachos')
+
+        name = models.CharField(_('Nombre'), max_length=50)
+
+        def __unicode__(self):
+            pass
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    firm = models.ForeignKey(Firm)
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
