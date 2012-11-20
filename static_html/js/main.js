51e9514d00cf96ad8f27465f2397b802dc387782
Francisco Caraballo <francisco.caraballo@gmail.com>
2012-11-20 15:21:26 +0100

    fix errors in cancel popover

--- static_html/js/main.js
+++ static_html/js/main.js
@@ -2,3 +2,7 @@ function trToggle(index_to_toggle) {
                 $(".hide-fast"+index_to_toggle).toggle();
                 $(".hide"+index_to_toggle).toggle('slow');
             }
+
+function hideAllPopovers() {
+                $("a[rel=popover]").popover('hide');
+            }
