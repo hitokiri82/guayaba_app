8105a4c5e10cb90dc654de04d5d331332ab78f75
Francisco Caraballo <francisco.caraballo@gmail.com>
2012-12-03 21:19:39 +0100

    Separated clients url file

---
+++ clients/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('clients.views',
+    url(r'^$', 'clients'),
+    url(r'^(?P<client_id>\d*)/$', 'client'),
+    url(r'^(?P<client_type>[^/]+)/basic/create/$', 'client_basic'),
+    url(r'^(?P<client_type>[^/]+)/basic/(?P<client_id>\d*)/$', 'client_basic'),
+    url(r'^(?P<client_id>\d*)/address/$', 'create_address'),
+    url(r'^(?P<client_id>\d*)/phones/$', 'phone_numbers'),
+    url(r'^(?P<client_id>\d*)/phones/(?P<operation>[^/]+)/(?P<phone_id>\d*)/$', 'phone_numbers'),
+
+)
